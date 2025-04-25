98e344cbccf5ffa68b046017032ff63c89740ec4
RuslanTopal <baranotik15@gmail.com>
2025-04-25 20:55:11 +0000

    add orders

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
 
 from cinema.serializers import (
     GenreSerializer,
@@ -11,7 +14,7 @@ from cinema.serializers import (
     MovieSessionListSerializer,
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
-    MovieListSerializer,
+    MovieListSerializer, OrderSerializer,
 )
 
 
@@ -56,3 +59,34 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        tickets_data = request.data.get("tickets", [])
+        if not tickets_data:
+            return Response(
+                {"error": "Tickets list is required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        order = Order.objects.create(user=request.user)
+        tickets = []
+
+        for ticket_data in tickets_data:
+            movie_session_id = ticket_data.pop("movie_session")
+            movie_session = get_object_or_404(MovieSession, id=movie_session_id)
+            tickets.append(
+                Ticket(order=order, movie_session=movie_session, **ticket_data)
+            )
+
+        Ticket.objects.bulk_create(tickets)
+        serializer = self.get_serializer(order)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -48,6 +48,14 @@ INSTALLED_APPS = [
     "user",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
@@ -124,7 +132,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
